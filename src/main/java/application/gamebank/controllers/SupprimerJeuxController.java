7e53471e16c5bce472916bd515f5d9b232bcfb04
DrakHardLord <tangi.beneat@gmail.com>
2024-06-03 16:24:47 +0200

    Complétion de la classe "NouveauTagController.java" + "SupprimerJeux.java"

--- src/main/java/application/gamebank/controllers/SupprimerJeuxController.java
+++ src/main/java/application/gamebank/controllers/SupprimerJeuxController.java
@@ -1,22 +1,36 @@
 package application.gamebank.controllers;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class SupprimerJeuxController {
 
-        @FXML
-        private Pane BoutonValiderSupressionJeu;
+    @FXML // Texte contenant le nom du jeu à supprimé
+    public Label game_name;
 
-        @FXML
-        private Label LabelNomJeuSupression;
+    // Variable définisant le fait de si oui ou non le jeu est supprimer
+    private boolean a_supprimer;
 
-        @FXML
-        void ActionValider(MouseEvent event) {
+    @FXML /** S'éxecute lors du chargement de la fenêtre */
+    void initialize() {
+        a_supprimer = false;
+    }
 
-        }
+    @FXML /** S'éxectue lors du renoncement à la supprésion */
+    void onCancelAction(MouseEvent event) {
+        quit();
+    }
 
+    @FXML /** S'éxecute lors de la validation de supprésion */
+    void onValideAction(MouseEvent event) {
+        a_supprimer = true;
+        quit();
+    }
+
+    /** Ferme la fenêtre */
+    private void quit() {
+        ((Stage) game_name.getScene().getWindow()).close();
+    }
 }
-
-
