a859f54593fbdd1bf7b2ebd72a294a1e6502e6d9
Louison Petit <louisonpetit67@gmail.com>
2024-06-03 14:12:09 +0200

    Réparation des fxml de louison et creation des controller, pour enlever les erreurs

---
+++ src/main/java/application/gamebank/NouveauTagController.java
@@ -0,0 +1,18 @@
+package application.gamebank;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class NouveauTagController {
+
+    @FXML
+    private TextField zoneTexteTag;
+
+    @FXML
+    void BoutonValiderNouveauTag(MouseEvent event) {
+
+    }
+
+}
---
+++ src/main/java/application/gamebank/SupprimerJeuxController.java
@@ -0,0 +1,22 @@
+package application.gamebank;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class SupprimerJeuxController {
+
+        @FXML
+        private Pane BoutonValiderSupressionJeu;
+
+        @FXML
+        private Label LabelNomJeuSupression;
+
+        @FXML
+        void ActionValider(MouseEvent event) {
+
+        }
+
+}
+
+
