038dc5f351204d5f964b125d500564b8fade64eb
DrakHardLord <tangi.beneat@gmail.com>
2024-06-03 14:53:59 +0200

    Création du début du code de l'accueil

--- src/main/java/application/gamebank/controllers/AccueilController.java
+++ src/main/java/application/gamebank/controllers/AccueilController.java
@@ -1,4 +1,12 @@
 package application.gamebank.controllers;
 
+import javafx.fxml.FXML;
+
 public class AccueilController {
+
+    @FXML
+    void initialize() {
+        // Code executé au lancement de l'application
+    }
+
 }
